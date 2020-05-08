38b55727e85a6d7c21a0c4f3982eeab79505d15b
Akash1420 <tripathiaakash14@gmail.com>
2020-05-08 21:41:43 +0530

    added methods in service

--- HuntAbode/src/app/services/rental.service.ts
+++ HuntAbode/src/app/services/rental.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { BehaviorSubject } from 'rxjs';
 
 const baseUrl = 'http://localhost:8001/';
 
@@ -42,7 +43,35 @@ export class RentalService {
       property,{observe: 'response'})
   }
 
+  updateStatusHome(homeid:number, status:string){
+      return this.http.put(baseUrl + 'home/updateByStatus/' + homeid+'/'+ status,
+      status,{observe : 'response'})
   }
+
+  updateOccupancyHome(homeId:number, occupancy: number){
+      return this.http.put(baseUrl+'home/updateByOccupancy/' + homeId+'/'+occupancy,
+      occupancy,{observe: 'response'})
+  }
+
+  updateSecurityCostHome(homeId:number, securityCost: number){
+    return this.http.put(baseUrl+'home/updateBySecurity/' + homeId+ '/' + securityCost,
+    securityCost,{observe: 'response'})
+  }
+
+  updatemonthlyCostHome(homeId:number, monthlyCost : number){
+    return this.http.put(baseUrl+'home/updateByCost/' + homeId+ '/' + monthlyCost,
+    monthlyCost,{observe: 'response'})
+  }
+
+  fetchHomeByLocation(homeId: number,location : string){
+    return this.http.get(baseUrl+'home/location/' + location )
+  }
+
+  fetchHomeByType(homeId: number, type: string){
+    return this.http.get(baseUrl + 'home/type/' + type)
+  }
+
+}
 
 
 
