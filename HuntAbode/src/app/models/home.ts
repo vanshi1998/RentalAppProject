381414971b745c6a4925a311a714a90615277567
Akash1420 <tripathiaakash14@gmail.com>
2020-05-10 23:00:11 +0530

    updated models

--- HuntAbode/src/app/models/home.ts
+++ HuntAbode/src/app/models/home.ts
@@ -1,2 +1,26 @@
 export class Home {
+    constructor(public id: number, public type: string, public location: string, public occupancy: string, public status: string, public furnished: string, public monthlyCost: number, public securityDeposit: number, public rooms: number, public details : string, public detailedLocation: string, public ownerId: number){
+
+    }
 }
+
+
+/*
+
+
+Data in spring-boot
+    int id;
+    String type;
+    String location;
+	String occupancy;
+	String status;
+	String furnished;
+	double monthlyCost;
+	double securityDeposit;
+	int rooms;
+	String details;
+	String detailedLocation;
+	int ownerId;
+
+
+*/
