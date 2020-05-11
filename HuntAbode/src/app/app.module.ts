1c973a27d50fb6517ba8e32eb358bdb81ca466f7
Akash1420 <tripathiaakash14@gmail.com>
2020-05-11 16:52:36 +0530

    added template

--- HuntAbode/src/app/app.module.ts
+++ HuntAbode/src/app/app.module.ts
@@ -1,72 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app.routing';
 
 import { AppComponent } from './app.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { LandingComponent } from './components/landing/landing.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
 
-import { AddPropertyComponent } from './components/add-property/add-property.component';
-import { ViewPropertyComponent } from './components/view-property/view-property.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { HeroComponent } from './components/hero/hero.component';
-import { GridComponent } from './components/grid/grid.component';
-import { InfoComponent } from './components/info/info.component';
-import { TenantHomeComponent } from './components/tenant-home/tenant-home.component';
-import { OwnerHomeComponent } from './components/owner-home/owner-home.component';
-import { OwnerDashboardComponent } from './components/owner-dashboard/owner-dashboard.component';
-import { AllPropertiesComponent } from './components/all-properties/all-properties.component';
-import { BookedPropertyComponent } from './components/booked-property/booked-property.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatGridListModule } from '@angular/material/grid-list';
-import { MatCardModule } from '@angular/material/card';
-import { MatMenuModule } from '@angular/material/menu';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { LayoutModule } from '@angular/cdk/layout';
-
-
-const appRoutes: Routes = [
- 
-  { path: 'viewProperty', component: ViewPropertyComponent },
-  { path: 'addProperty' , component: AddPropertyComponent},
-  { path: 'ownerHome' , component: OwnerHomeComponent},
-  { path: 'tenantHome' , component: TenantHomeComponent},
-  { path: 'allProperties' , component: AllPropertiesComponent},
-  { path: 'bookedProperties' , component: BookedPropertyComponent},
-  { path: 'ownerDashboard' , component: OwnerDashboardComponent},
- 
-];
+import { HomeModule } from './components/home/home.module';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddPropertyComponent,
-    ViewPropertyComponent,
-    FooterComponent,
+    SignupComponent,
+    LandingComponent,
+    ProfileComponent,
     NavbarComponent,
-    HeroComponent,
-    GridComponent,
-    InfoComponent,
-    TenantHomeComponent,
-    OwnerHomeComponent,
-    OwnerDashboardComponent,
-    AllPropertiesComponent,
-    BookedPropertyComponent
+    FooterComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
+    NgbModule,
+    FormsModule,
     RouterModule,
-    BrowserAnimationsModule,
-    MatGridListModule,
-    MatCardModule,
-    MatMenuModule,
-    MatIconModule,
-    MatButtonModule,
-    LayoutModule,
-    RouterModule.forRoot(
-      appRoutes,
-      { enableTracing: true } // <-- debugging purposes only
-    )
+    AppRoutingModule,
+    HomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
