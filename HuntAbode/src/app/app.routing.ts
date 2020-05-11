3c8630e35ce23bed391194f2930e599b6daac286
Ikshita Srivastava <charvie19june@gmail.com>
2020-05-12 01:07:28 +0530

    Added Landing Page
    HomePage added along with owner profile component

--- HuntAbode/src/app/app.routing.ts
+++ HuntAbode/src/app/app.routing.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule, } from '@angular/common';
-import { BrowserModule  } from '@angular/platform-browser';
+import { BrowserModule } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './components/home/home.component';
@@ -8,27 +8,27 @@ import { ProfileComponent } from './components/profile/profile.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { LandingComponent } from './components/landing/landing.component';
 import { LoginComponent } from './components/login/login.component';
-import {AddPropertyComponent } from './components/add-property/add-property.component';
-import {ViewPropertyComponent} from './components/view-property/view-property.component';
-import {ViewPropertyOwnerComponent} from './components/view-property-owner/view-property-owner.component';
+import { AddPropertyComponent } from './components/add-property/add-property.component';
+import { ViewPropertyComponent } from './components/view-property/view-property.component';
+import { ViewPropertyOwnerComponent } from './components/view-property-owner/view-property-owner.component';
 
-const routes: Routes =[
-    { path: 'home',             component: HomeComponent },
-    { path: 'add-property',             component: AddPropertyComponent },
-    { path: 'user-profile',     component: ProfileComponent },
-    { path: 'register',           component: SignupComponent },
-    { path: 'landing',          component: LandingComponent },
-    { path: 'login',          component: LoginComponent },
-    { path: 'view-property', component: ViewPropertyComponent},
-    { path: 'view-property-owner', component: ViewPropertyOwnerComponent},
-    { path: '', redirectTo: 'view-property-owner', pathMatch: 'full' }
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'add-property', component: AddPropertyComponent },
+  { path: 'user-profile', component: ProfileComponent },
+  { path: 'register', component: SignupComponent },
+  { path: 'landing', component: LandingComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'view-property', component: ViewPropertyComponent },
+  { path: 'view-property-owner', component: ViewPropertyOwnerComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
 
 @NgModule({
   imports: [
     CommonModule,
     BrowserModule,
-    RouterModule.forRoot(routes,{
+    RouterModule.forRoot(routes, {
       useHash: false
     })
   ],
