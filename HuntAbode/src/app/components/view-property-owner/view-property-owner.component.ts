4e75b619528f7a3f542fbfdf723d2a57fb2ab763
Akash1420 <tripathiaakash14@gmail.com>
2020-05-13 00:25:49 +0530

    added backend in component

--- HuntAbode/src/app/components/view-property-owner/view-property-owner.component.ts
+++ HuntAbode/src/app/components/view-property-owner/view-property-owner.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {RentalService} from 'src/app/services/rental.service';
+import { Router, ActivatedRoute } from '@angular/router';
+
+
 
 @Component({
   selector: 'app-view-property-owner',
@@ -7,9 +11,73 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewPropertyOwnerComponent implements OnInit {
 
-  constructor() { }
+  homes:Array<any>=[]
+  email : string='ikshita@gmail.com';
+
+
+  constructor(private rentalService: RentalService ,private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    console.log('check!');
+
+    this.rentalService.fetchPropertryCertain(this.email)
+    .subscribe((res: Array<any>)=>{
+      console.log(res);
+      this.homes=res;
+    })
+  }
+
+  deleteProperty(){
+    /* to be implemented  */
+
   }
 
 }
+
+
+/*
+
+import { Component, OnInit } from '@angular/core';
+import {SurveyService} from 'src/app/services/survey.service';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Survey } from 'src/app/models/survey';
+import { TakeSurveyComponent } from '../take-survey/take-survey.component';
+import { Question } from 'src/app/models/question';
+
+@Component({
+  selector: 'app-view-surveys',
+  templateUrl: './view-surveys.component.html',
+  styleUrls: ['./view-surveys.component.css']
+})
+export class ViewSurveysComponent implements OnInit {
+
+  surveys:Array<Survey>=[]
+  surveyName: string;
+  questions: Array<Question>=[]
+
+  constructor(private surveyService : SurveyService, private router: Router, private route: ActivatedRoute) { }
+
+  
+
+
+
+ngOnInit(): void {
+  this.surveyService.fetchAllSurveys()
+  .subscribe((res:Array<Survey>)=> {
+    console.log(res);
+    this.surveys = res;
+  })
+  
+}
+
+takeSurvey(choices:any){
+  console.log("choices :",choices);
+  this.router.navigate(["takeSurvey",{name:choices.name}]);
+}
+
+
+
+}
+
+*/
+
