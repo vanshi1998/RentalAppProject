6320a955f15b5ca14604ce26f13c372b46b135d9
Shreyajha98 <jha.shreya98@gmail.com>
2020-05-12 23:29:48 +0530

    owner and tenant

--- HuntAbode/src/app/components/owner-home/owner-home.component.ts
+++ HuntAbode/src/app/components/owner-home/owner-home.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Inject } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { FormControl } from '@angular/forms';
+import { Home } from "src/app/models/home";
+import { FormBuilder, FormArray, Validators } from '@angular/forms';
+import { RentalService } from "src/app/services/rental.service";
 
 @Component({
   selector: 'app-owner-home',
@@ -7,9 +12,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OwnerHomeComponent implements OnInit {
 
-  constructor() { }
+  
+  focus;
+  focus1;
+  focus2;
+  ownerForm:FormGroup;
+  constructor(private fb: FormBuilder,private rentalService:RentalService) { }
 
+  gemail:string;
   ngOnInit(): void {
+    this.ownerForm = this.fb.group({
+      name: ['', Validators.required],
+      email: ['', Validators.required],
+      contact: ['', Validators.required],
+      
+    
+});
   }
+  
+  onSubmit(){
+    //console.log("form value=", this.ownerForm.value);
+    this.rentalService.addHouseOwner(this.ownerForm.value,this.ownerForm.value.email)
+            .subscribe(res=>{
+              console.log(res)
+               // this.router.navigate()
+            });
+          
+          }
 
 }
