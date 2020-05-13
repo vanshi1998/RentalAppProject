9391c0c10c861951e0fad5fc6767f262cad66759
Ikshita Srivastava <charvie19june@gmail.com>
2020-05-13 18:15:15 +0530

    DatePicker Added

--- HuntAbode/src/app/components/view-detail-property/view-detail-property.component.ts
+++ HuntAbode/src/app/components/view-detail-property/view-detail-property.component.ts
@@ -12,63 +12,78 @@ import { NgbDate, NgbCalendar } from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./view-detail-property.component.css']
 })
 export class ViewDetailPropertyComponent implements OnInit {
-  
-  home:Home;
-    id:any;
-    closeResult: string;
 
-  constructor(private modalService: NgbModal,calendar: NgbCalendar,private rentalService:RentalService, private router:Router, private route:ActivatedRoute) { }
+  home: Home;
+  id: any;
+  fromDate: NgbDate;
+  toDate: NgbDate;
+  hoveredDate: NgbDate;
+  closeResult: string;
+  model1: NgbDate;
+  model2: NgbDate;
+
+  focus;
+  focus1;
+  focus2;
+  focus3;
+  focus4;
+
+  constructor(private modalService: NgbModal, calendar: NgbCalendar, private rentalService: RentalService, private router: Router, private route: ActivatedRoute) {
+
+  }
 
   ngOnInit(): void {
 
     this.route.paramMap.subscribe(params => {
-      
+
       console.log('***', params.get('id'));
-      this.id=params.get('id');
-      console.log("Id is",this.id);
+      this.id = params.get('id');
+      console.log("Id is", this.id);
+
+
     })
 
 
 
     this.rentalService.fetchPropertyById(this.id).subscribe((res: Home) => {
-      
+
       this.home = res;
-      console.log("Home is=",this.home);
-      })
-    
+      console.log("Home is=", this.home);
+    })
+
   }
 
   open(content, type, modalDimension) {
     if (modalDimension === 'sm' && type === 'modal_mini') {
-        this.modalService.open(content, { windowClass: 'modal-mini', size: 'sm', centered: true }).result.then((result) => {
-            this.closeResult = `Closed with: ${result}`;
-        }, (reason) => {
-            this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-        });
+      this.modalService.open(content, { windowClass: 'modal-mini', size: 'sm', centered: true }).result.then((result) => {
+        this.closeResult = `Closed with: ${result}`;
+      }, (reason) => {
+        this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+      });
     } else if (modalDimension === '' && type === 'Notification') {
       this.modalService.open(content, { windowClass: 'modal-danger', centered: true }).result.then((result) => {
-          this.closeResult = `Closed with: ${result}`;
+        this.closeResult = `Closed with: ${result}`;
       }, (reason) => {
-          this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+        this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
       });
     } else {
-        this.modalService.open(content,{ centered: true }).result.then((result) => {
-            this.closeResult = `Closed with: ${result}`;
-        }, (reason) => {
-            this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
-        });
+      this.modalService.open(content, { centered: true }).result.then((result) => {
+        this.closeResult = `Closed with: ${result}`;
+      }, (reason) => {
+        this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+      });
     }
-}
-
-
-private getDismissReason(reason: any): string {
-  if (reason === ModalDismissReasons.ESC) {
-      return 'by pressing ESC';
-  } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
-      return 'by clicking on a backdrop';
-  } else {
-      return  `with: ${reason}`;
   }
-}
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
+  }
+
 
 }
