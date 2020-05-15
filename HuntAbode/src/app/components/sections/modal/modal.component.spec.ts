b825356eb7455f99f8cf0e3f45278afb4f943cd3
Shreyajha98 <jha.shreya98@gmail.com>
2020-05-15 11:27:07 +0530

    modal component

--- HuntAbode/src/app/components/sections/modal/modal.component.spec.ts
+++ HuntAbode/src/app/components/sections/modal/modal.component.spec.ts
@@ -1,20 +1,22 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ModalComponent } from './modal.component';
+import { NgbdModalComponent } from './modal.component';
+//import { ModalComponent } from 'ng2-bs3-modal/ng2-bs3-modal';
+//import {ModalComponent} from 'src/app/components/sections/modal/modal.component';
 
 describe('ModalComponent', () => {
-  let component: ModalComponent;
-  let fixture: ComponentFixture<ModalComponent>;
+  let component: NgbdModalComponent;
+  let fixture: ComponentFixture<NgbdModalComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ModalComponent ]
+      declarations: [ NgbdModalComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ModalComponent);
+    fixture = TestBed.createComponent(NgbdModalComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
