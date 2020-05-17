0fb3293a7baf45aa29acde43fdb47f0e498bb51b
Ikshita Srivastava <charvie19june@gmail.com>
2020-05-17 22:55:35 +0530

    Filtering Added

---
+++ HuntAbode/src/app/components/filter-furnishing/filter-furnishing.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-filter-furnishing',
+  templateUrl: './filter-furnishing.component.html',
+  styleUrls: ['./filter-furnishing.component.css']
+})
+export class FilterFurnishingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ HuntAbode/src/app/components/filter-occupancy/filter-occupancy.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-filter-occupancy',
+  templateUrl: './filter-occupancy.component.html',
+  styleUrls: ['./filter-occupancy.component.css']
+})
+export class FilterOccupancyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
