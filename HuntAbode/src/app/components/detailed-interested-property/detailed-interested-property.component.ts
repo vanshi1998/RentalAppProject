cac8d641a8ad7bd01144c0a72cbbd10f4e2bffcd
Ikshita Srivastava <charvie19june@gmail.com>
2020-05-18 22:39:57 +0530

    Carousel Added

--- HuntAbode/src/app/components/detailed-interested-property/detailed-interested-property.component.ts
+++ HuntAbode/src/app/components/detailed-interested-property/detailed-interested-property.component.ts
@@ -10,28 +10,29 @@ import { RentalService } from 'src/app/services/rental.service';
 })
 export class DetailedInterestedPropertyComponent implements OnInit {
 
-    id:any;
-    approve:string;
-    home:Home;
+  id: any;
+  approve: string;
+  home: Home;
+  imgs: Array<string> = ["https://images.pexels.com/photos/584399/living-room-couch-interior-room-584399.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", "https://images.pexels.com/photos/271624/pexels-photo-271624.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", "https://images.pexels.com/photos/439227/pexels-photo-439227.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"];
 
-  constructor(private rentalService:RentalService,private route: ActivatedRoute) { }
+  constructor(private rentalService: RentalService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
 
     this.route.paramMap.subscribe(params => {
-      
+
       console.log('***', params.get('id'));
       this.approve = params.get("approve");
-      this.id=params.get('id');
-      console.log("Id is",this.id);
-      console.log("Approve is=",this.approve);
+      this.id = params.get('id');
+      console.log("Id is", this.id);
+      console.log("Approve is=", this.approve);
     })
 
     this.rentalService.fetchPropertyById(this.id).subscribe((res: Home) => {
-      
+
       this.home = res;
-      console.log("Home are=",this.home);
-      })
+      console.log("Home are=", this.home);
+    })
 
   }
 
