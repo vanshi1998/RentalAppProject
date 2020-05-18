30f556da3835806cd76a58fc5a078b9e52c2806f
Ikshita Srivastava <charvie19june@gmail.com>
2020-05-19 01:05:22 +0530

    BookedProperty Component Updated

--- HuntAbode/src/app/components/booked-property/booked-property.component.ts
+++ HuntAbode/src/app/components/booked-property/booked-property.component.ts
@@ -3,6 +3,7 @@ import { RentalService } from "src/app/services/rental.service";
 import { InterestedHome } from "src/app/models/interested-home";
 import { Home } from "src/app/models/home";
 import { Router, ActivatedRoute } from '@angular/router';
+import { NgbModal, ModalDismissReasons } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-booked-property',
@@ -11,50 +12,53 @@ import { Router, ActivatedRoute } from '@angular/router';
 })
 export class BookedPropertyComponent implements OnInit {
 
-  constructor(private rentalService:RentalService,private router: Router, private route: ActivatedRoute)  { }
+  imgs: Array<string> = ["https://images.pexels.com/photos/584399/living-room-couch-interior-room-584399.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", "https://images.pexels.com/photos/271624/pexels-photo-271624.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500", "https://images.pexels.com/photos/439227/pexels-photo-439227.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"];
+  closeResult: string;
+  constructor(private rentalService: RentalService, private router: Router, private route: ActivatedRoute) { }
 
-  email :string;
-  interestedHomes:Array<InterestedHome>;
-homeIds:Array<number>=[];
-approves:Array<String>=[];
-requiredApprove:String;
-intHomes:Array<Home>=[];
-id:number;
+  email: string;
+  interestedHomes: Array<InterestedHome>;
+  homeIds: Array<number> = [];
+  approves: Array<String> = [];
+  requiredApprove: String;
+  intHomes: Array<Home> = [];
+  id: number;
   ngOnInit(): void {
 
-   //this.email="abcd@gmail.com";
+    //this.email="abcd@gmail.com";
 
-   this.route.paramMap.subscribe(params => {
-      
-    console.log('***', params.get('email'));
-    this.email=params.get('email');
-    console.log("Email=",this.email);
-    
-  })
+    this.route.paramMap.subscribe(params => {
 
-    console.log("Email=",this.email);
+      console.log('***', params.get('email'));
+      this.email = params.get('email');
+      console.log("Email=", this.email);
+
+    })
+
+    console.log("Email=", this.email);
     this.rentalService.fetchInterestedHomes(this.email)
-  .subscribe((res:Array<InterestedHome>)=> {
-    console.log("result",res);
-    this.interestedHomes = res;
-    console.log("Interested Homes are",this.interestedHomes);
-   })
+      .subscribe((res: Array<InterestedHome>) => {
+        console.log("result", res);
+        this.interestedHomes = res;
+        console.log("Interested Homes are", this.interestedHomes);
+      })
 
 
- setTimeout(() => { this.getdata();  }, 1000);
+    setTimeout(() => { this.getdata(); }, 1000);
 
 
-}
+  }
 
-getdata()
-{
-   this.interestedHomes.forEach(interestHome => {
+
+
+  getdata() {
+    this.interestedHomes.forEach(interestHome => {
       this.homeIds.push(interestHome.homeId);
       this.approves.push(interestHome.approve);
     });
 
-   console.log("All home ids=",this.homeIds);
-   
+    console.log("All home ids=", this.homeIds);
+
     /* this.homeIds.forEach(id => {
       this.rentalService.fetchPropertyById(id)
   .subscribe((res:Home)=> {
@@ -63,21 +67,20 @@ getdata()
     console.log("Home is",this.intHomes);
     });
    }) */
-   this.rentalService.fetchHomesOfInterestedHomes(this.email)
-   .subscribe((res:Array<Home>)=> {
-     console.log("result",res);
-    this.intHomes=res;
-     console.log("Homes are",this.intHomes);
-    })
+    this.rentalService.fetchHomesOfInterestedHomes(this.email)
+      .subscribe((res: Array<Home>) => {
+        console.log("result", res);
+        this.intHomes = res;
+        console.log("Homes are", this.intHomes);
+      })
 
-}
+  }
 
-loadDetails(i:number)
-{
-  this.requiredApprove=this.approves[i];
-  this.id=this.homeIds[i];
-  this.router.navigate(["detailed-interested-property",{id:this.id,approve:this.requiredApprove}]);
-}
+  loadDetails(i: number) {
+    this.requiredApprove = this.approves[i];
+    this.id = this.homeIds[i];
+    this.router.navigate(["detailed-interested-property", { id: this.id, approve: this.requiredApprove }]);
+  }
 
 }
 
